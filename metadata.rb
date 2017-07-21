e0b00501afce17d720592acb05cabb018264d2fe
Mandi Walls <mandi.walls@gmail.com>
2017-07-21 14:13:10 +0100

    initial commit

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,8 @@ long_description 'Installs/Configures aar-habitat'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
+depends 'habitat','~> 0.4.0'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
